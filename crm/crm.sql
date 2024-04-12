b3c7fc309cd48c559142e950eae4de6cd14e67f1
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-04-12 16:34:47 +0000

    class work

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,47 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS sales_teams;
 
+CREATE TABLE salespersons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    sales_team_id INTEGER
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT,
+    company.id INTEGER
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    salesperson_id INTEGER,
+    contact_id INTEGER, 
+    activity_date date,
+    activity_time time, 
+    activity_type TEXT,
+    notes TEXT,
+    company_id INTEGER
+    -- include company here in case contact changes company
+);
+
+CREATE TABLE sales_teams (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+);
 -- CREATE TABLES
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,43 @@
 
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS engagements;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS sales_teams;
+
+
+CREATE TABLE users (
+    id
+    first_name
+    last_name
+    email
+    username
+    phone_number
+
+);
+
+CREATE TABLE posts (
+    id
+    user_id
+    post_url
+    posted_at datetime, 
+    caption TEXT
+    total_likes --counter_cache 
+
+); 
+
+CREATE table likes (
+    id
+    post_id INTEGER,
+    user_id INTEGER
+    -- logic to limit one like per post per user
+
+);
+
+CREATE TABLE comments (
+    id 
+    post_id
+    user_id
+    comment_text
+    
+)
